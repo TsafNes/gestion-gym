2da65e6edefa656416316bd1ca9a60a69d0cd81b
Tsafack Nestor <nestor.tsafack@yahoo.fr>
2025-07-03 08:20:15 -0400

    Fix JWT endpoint

--- gym_backend/settings.py
+++ gym_backend/settings.py
@@ -36,9 +36,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'rest_framework',
-    'corsheaders',
-    'rest_framework_simplejwt',  
+    
 ]
 
 ROOT_URLCONF = 'gym_backend.urls'
