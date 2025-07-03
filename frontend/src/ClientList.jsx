effbe76926286244608bc0426619a0eaedae3935
Tsafack Nestor <nestor.tsafack@yahoo.fr>
2025-07-03 13:51:24 -0400

    Ajout

--- frontend/src/ClientList.jsx
+++ frontend/src/ClientList.jsx
@@ -1,27 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-export function ClientList({ refreshTrigger }) {
-  const [clients, setClients] = useState([]);
-  const token = localStorage.getItem('access_token');
-
-  const fetchClients = async () => {
-    try {
-      const response = await axios.get('https://gestion-gym.onrender.com/api/clients/', {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      setClients(response.data);
-    } catch (error) {
-      console.error("Erreur lors de la récupération des clients :", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchClients();
-  }, [refreshTrigger]);
-
+function ClientList({ clients }) {
   return (
     <div className="client-list">
       <h2>👥 Liste des clients</h2>
@@ -39,3 +19,5 @@ export function ClientList({ refreshTrigger }) {
     </div>
   );
 }
+
+export default ClientList;
